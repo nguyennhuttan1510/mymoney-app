acb73d25029a5818f80ff0c49127fd3671b7e36a
Tan Nguyen <nhuttan.nguyen@mafc.com.vn>
2025-01-17 20:23:39 +0700

    [Commit]

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,45 +1,94 @@
 import { Tabs } from 'expo-router';
 import React from 'react';
-import { Platform } from 'react-native';
+import {Platform, View} from 'react-native';
 
 import { HapticTab } from '@/components/HapticTab';
 import { IconSymbol } from '@/components/ui/IconSymbol';
 import TabBarBackground from '@/components/ui/TabBarBackground';
 import { Colors } from '@/constants/Colors';
 import { useColorScheme } from '@/hooks/useColorScheme';
+import MaterialIcons from "@expo/vector-icons/MaterialIcons";
+import {Ionicons, MaterialCommunityIcons} from "@expo/vector-icons";
+import {useTheme} from "@react-navigation/native";
 
 export default function TabLayout() {
-  const colorScheme = useColorScheme();
+    const colorScheme = useColorScheme();
+    const {colors} = useTheme()
+      return (
+          <Tabs screenOptions={{tabBarStyle: {
+                  height: 50,                // Height of the tab bar
+                  paddingBottom: 8,          // Padding at the bottom of the tab bar
+                  backgroundColor: 'white', // Background color of the tab bar
+              },}}>
+              <Tabs.Screen
+                  options={{
+                      headerShown: false,
+                      title: 'Home',
+                      tabBarIcon: ({color, focused}) => <MaterialIcons size={24} name='home' color={color} />}}
+                  name='index'
+              />
+              <Tabs.Screen
+                  options={{
+                      headerShown: false,
+                      title: 'Transaction',
+                      tabBarIcon: ({color, focused}) => <MaterialCommunityIcons name="swap-horizontal" size={24} color={color} />}}
+                  name='transaction'
+              />
+              <Tabs.Screen
+                  options={{
+                      headerShown: false,
+                      tabBarLabel: '',
+                      tabBarIcon: ({color, focused}) => (
+                          <View style={{display: 'flex', justifyContent: 'center', alignItems:'center', backgroundColor: colors.background, borderRadius: 100, height: 64, width:64, marginBottom: 8}}>
+                              <Ionicons name="add-circle-sharp" size={50} color={'#fdc500'} />
+                          </View>
+                      )}}
+                  name='transaction-create'
+              />
+              <Tabs.Screen
+                  options={{
+                      headerShown: false,
+                      title: 'Budget',
+                      tabBarIcon: ({color, focused}) => <MaterialIcons name="attach-money" size={24} color={color} />}}
+                  name='budget'
+              />
+              <Tabs.Screen
+                  options={{
+                      headerShown: false,
+                      title: 'Person',
+                      tabBarIcon: ({color, focused}) => <Ionicons name="person-circle-outline" size={24} color={color} />}}
+                  name='personal'
+              />
+          </Tabs>
 
-  return (
-    <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-        headerShown: false,
-        tabBarButton: HapticTab,
-        tabBarBackground: TabBarBackground,
-        tabBarStyle: Platform.select({
-          ios: {
-            // Use a transparent background on iOS to show the blur effect
-            position: 'absolute',
-          },
-          default: {},
-        }),
-      }}>
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
-        }}
-      />
-      <Tabs.Screen
-        name="explore"
-        options={{
-          title: 'Explore',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
-        }}
-      />
-    </Tabs>
-  );
+        // <Tabs
+        //   screenOptions={{
+        //     tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
+        //     headerShown: false,
+        //     tabBarButton: HapticTab,
+        //     tabBarBackground: TabBarBackground,
+        //     tabBarStyle: Platform.select({
+        //       ios: {
+        //         // Use a transparent background on iOS to show the blur effect
+        //         position: 'absolute',
+        //       },
+        //       default: {},
+        //     }),
+        //   }}>
+        //   <Tabs.Screen
+        //     name="index"
+        //     options={{
+        //       title: 'Home',
+        //       tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
+        //     }}
+        //   />
+        //   <Tabs.Screen
+        //     name="explore"
+        //     options={{
+        //       title: 'Explore',
+        //       tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
+        //     }}
+        //   />
+        // </Tabs>
+      );
 }
--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,56 +1,120 @@
-import { Image, StyleSheet, Platform } from 'react-native';
-
+import {Image, StyleSheet, Platform, SafeAreaView, View, Text, ScrollView} from 'react-native';
+import tinyColor from 'tinycolor2';
 import { HelloWave } from '@/components/HelloWave';
 import ParallaxScrollView from '@/components/ParallaxScrollView';
 import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
+import {useTheme} from "@react-navigation/native";
+import {Link} from "expo-router";
 
 export default function HomeScreen() {
+    const {colors} = useTheme()
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({
-              ios: 'cmd + d',
-              android: 'cmd + m',
-              web: 'F12'
-            })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          Tap the Explore tab to learn more about what's included in this starter app.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          When you're ready, run{' '}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
+      <SafeAreaView style={{flex: 1}}>
+          <View className='p-2 h-fit'>
+              <View className='bg-[#1d1e20] p-4 rounded-3xl mb-4'>
+                  <View className='flex flex-row gap-2 mb-4'>
+                      <Image source={require('@/assets/images/partial-react-logo.png')} className='w-10 h-10 rounded-full mb-1'/>
+                      <View>
+                          <Text style={{color: colors.text}} className='text-lg'>Nguyen Nhut Tan</Text>
+                          <Text style={{color: tinyColor(colors.text).darken(40).toString()}}>nguyentan1510</Text>
+                      </View>
+                  </View>
+
+                  <View className='mb-4 flex flex-row justify-between items-baseline'>
+                      <Text style={{color: tinyColor(colors.text).darken(40).toString()}} className='text-lg'>Total</Text>
+                      <Text style={{color: colors.text}} className='text-2xl'>$9,780.39</Text>
+                  </View>
+
+                  <View className='flex flex-row justify-between'>
+                      <Text style={{color: colors.text}} className='text-xl'>Statistics</Text>
+                      <View className='flex flex-row gap-2 items-center'>
+                          <Text style={{color: colors.text}} className='text-sm'>Budget</Text>
+                          <View className='flex flex-row gap-1 items-center rounded-full bg-gray-700'>
+                              <Text style={{color: colors.text}} className='pl-1 pr-2 py-1'>Personal</Text>
+                          </View>
+                      </View>
+                  </View>
+              </View>
+          </View>
+
+          <View className='bg-[#1d1e20] p-4 rounded-tl-3xl rounded-tr-3xl flex-1'>
+              <View className='flex flex-row justify-between mb-4'>
+                  <Text style={{color: colors.text}} className='font-bold'>Transaction</Text>
+                  <Link href={'/'} style={{color: colors.text}} className='font-bold'>View All</Link>
+              </View>
+
+              <ScrollView>
+                  <View className='flex flex-col gap-y-2'>
+                      {/*<Transaction />*/}
+
+                      <View className='flex flex-col gap-2'>
+                          <Text style={{color: colors.text}}>Yesterday</Text>
+                          <View className='flex flex-row gap-2 rounded-2xl border p-2' style={{borderColor: colors.border}}>
+                              <Image source={require('@/assets/images/partial-react-logo.png')} className='w-8 h-8 rounded-full'/>
+                              <View>
+                                  <Text style={{color: colors.text}} className='text-md font-bold'>Food</Text>
+                                  <Text style={{color: tinyColor(colors.text).darken(40).toString()}} className='text-sm'>Dinner</Text>
+                              </View>
+                              <Text style={{color: colors.text}} className='flex-1 text-right'>-$39</Text>
+                          </View>
+
+                          <View className='flex flex-row gap-2 rounded-2xl border p-2' style={{borderColor: colors.border}}>
+                              <Image source={require('@/assets/images/partial-react-logo.png')} className='w-8 h-8 rounded-full'/>
+                              <View>
+                                  <Text style={{color: colors.text}} className='text-md font-bold'>Food</Text>
+                                  <Text style={{color: tinyColor(colors.text).darken(40).toString()}} className='text-sm'>Dinner</Text>
+                              </View>
+                              <Text style={{color: colors.text}} className='flex-1 text-right'>-$39</Text>
+                          </View>
+                      </View>
+
+                      <View className='flex flex-col gap-2'>
+                          <Text style={{color: colors.text}}>6/9</Text>
+                          <View className='flex flex-row gap-2 rounded-2xl border p-2' style={{borderColor: colors.border}}>
+                              <Image source={require('@/assets/images/partial-react-logo.png')} className='w-8 h-8 rounded-full'/>
+                              <View>
+                                  <Text style={{color: colors.text}} className='text-md font-bold'>Food</Text>
+                                  <Text style={{color: tinyColor(colors.text).darken(40).toString()}} className='text-sm'>Dinner</Text>
+                              </View>
+                              <Text style={{color: colors.text}} className='flex-1 text-right'>-$39</Text>
+                          </View>
+
+                          <View className='flex flex-row gap-2 rounded-2xl border p-2' style={{borderColor: colors.border}}>
+                              <Image source={require('@/assets/images/partial-react-logo.png')} className='w-8 h-8 rounded-full'/>
+                              <View>
+                                  <Text style={{color: colors.text}} className='text-md font-bold'>Food</Text>
+                                  <Text style={{color: tinyColor(colors.text).darken(40).toString()}} className='text-sm'>Dinner</Text>
+                              </View>
+                              <Text style={{color: colors.text}} className='flex-1 text-right'>-$39</Text>
+                          </View>
+                      </View>
+
+                      <View className='flex flex-col gap-2'>
+                          <Text style={{color: colors.text}}>5/9</Text>
+                          <View className='flex flex-row gap-2 rounded-2xl border p-2' style={{borderColor: colors.border}}>
+                              <Image source={require('@/assets/images/partial-react-logo.png')} className='w-8 h-8 rounded-full'/>
+                              <View>
+                                  <Text style={{color: colors.text}} className='text-md font-bold'>Food</Text>
+                                  <Text style={{color: tinyColor(colors.text).darken(40).toString()}} className='text-sm'>Dinner</Text>
+                              </View>
+                              <Text style={{color: colors.text}} className='flex-1 text-right'>-$39</Text>
+                          </View>
+
+                          <View className='flex flex-row gap-2 rounded-2xl border p-2' style={{borderColor: colors.border}}>
+                              <Image source={require('@/assets/images/partial-react-logo.png')} className='w-8 h-8 rounded-full'/>
+                              <View>
+                                  <Text style={{color: colors.text}} className='text-md font-bold'>Food</Text>
+                                  <Text style={{color: tinyColor(colors.text).darken(40).toString()}} className='text-sm'>Dinner</Text>
+                              </View>
+                              <Text style={{color: colors.text}} className='flex-1 text-right'>-$39</Text>
+                          </View>
+                      </View>
+                  </View>
+              </ScrollView>
+
+          </View>
+      </SafeAreaView>
   );
 }
 
